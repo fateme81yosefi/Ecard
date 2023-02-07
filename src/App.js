99839c0c02d3000c17cf8ec14387166f06038dae
Fateme Yosefi <86098248+fateme81yosefi@users.noreply.github.com>
2023-02-07 12:15:06 +0330

    last commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from "react";
+import Home from "./components/Home/Home";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { stringify } from "ajv";
+import { DataProvider } from "./components/shared/Shared";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {" "}
+        <DataProvider>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/parhamKish">
+            <Home />
+          </Route>{" "}
+        </DataProvider>
+      </Switch>
+    </BrowserRouter>
+  );
+}
---
+++ src/components/shared/Shared.js
@@ -0,0 +1,14 @@
+import React, { createContext, useEffect, useState } from "react";
+export const DataContext = createContext();
+
+export const DataProvider = (props) => {
+  
+
+  let [Data, setData] = useState({});
+
+  return (
+    <DataContext.Provider value={[Data, setData]}>
+      {props.children}
+    </DataContext.Provider>
+  );
+};
